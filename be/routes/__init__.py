09213415759fc5f0dbc163db46ea2c3e8a22a481
Apple <apple@Apples-MacBook-Air.local>
2023-11-23 22:25:24 +0530

    namespace changes

--- be/routes/__init__.py
+++ be/routes/__init__.py
@@ -1,9 +1,9 @@
-from .user import router as user_router
-from .package import router as package_router
-from .apartment import router as apartment_router
+from .users import router as user_router
+from .packages import router as package_router
+from .apartments import router as apartment_router
 
 
 def register_routes(app):
-    app.include_router(user_router, tags=["user"], prefix="/user")
-    app.include_router(package_router, tags=["package"], prefix="/package")
-    app.include_router(apartment_router, tags=["apartment"], prefix="/apartment")
+    app.include_router(user_router, tags=["users"], prefix="/users")
+    app.include_router(package_router, tags=["packages"], prefix="/packages")
+    app.include_router(apartment_router, tags=["apartments"], prefix="/apartments")
