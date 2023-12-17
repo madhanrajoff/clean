858c4777a03a70f15d83cbccded15cb1dd2aa964
Apple <apple@Apples-MacBook-Air.local>
2023-12-18 01:05:16 +0530

    PWA changes

--- argon-design-system-react/src/index.js
+++ argon-design-system-react/src/index.js
@@ -29,6 +29,9 @@ import Login from "views/examples/Login.js";
 import Profile from "views/examples/Profile.js";
 import Register from "views/examples/Register.js";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -43,3 +46,13 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
